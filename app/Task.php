f3f13903501b92384e894139393fa3f370020a4e
ntrv <hydrangea56@me.com>
2019-07-30 13:23:35 +0900

    Enable SoftDeletes

--- app/Task.php
+++ app/Task.php
@@ -2,9 +2,19 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $dates = [
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'name',
+    ];
 }
